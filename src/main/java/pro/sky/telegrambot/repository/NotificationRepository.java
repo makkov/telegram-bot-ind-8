fe25e81899dc74cb4dfb2ceee075867c260f0e6a
maks <maxim_86nva@mail.ru>
2023-10-28 11:08:30 +0300

    отбор и отправка задач по шедулеру

--- src/main/java/pro/sky/telegrambot/repository/NotificationRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambot.entity.Notification;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findAllByExecDate(LocalDateTime execDate);
 }
