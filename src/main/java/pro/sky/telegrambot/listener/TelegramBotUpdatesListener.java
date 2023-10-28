32e425e25baf6e0c383ccf76b73555fd42756cb7
maks <maxim_86nva@mail.ru>
2023-10-28 10:26:59 +0300

    обработка /start от тг

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +30,12 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            Long chatId = update.message().chat().id();
+            if (update.message().text().equals("/start")) {
+                SendMessage message = new SendMessage(chatId, "Добро пожаловать, введите задачу. Пример формата: 01.01.2022 20:00 Сделать домашнюю работу");
+                SendResponse response = telegramBot.execute(message);
+            }
+            logger.info("Сообщение из телеги: " + update.message().text());
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
